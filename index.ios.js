3a33b9b4a58a93ade18bf3d8169f43f5f98b4bf1
Jarek Potiuk <jarek@potiuk.com>
2015-08-26 16:35:00 +0200

    Added non-animated image from assets.

--- index.ios.js
+++ index.ios.js
@@ -10,49 +10,31 @@ var {
   StyleSheet,
   Text,
   View,
+  Image
 } = React;
 
-// TODO(TASK2): create .xcassets folder and add images to it (XCode)
 var CodepotReact = React.createClass({
   render: function() {
     return (
-      // TODO(TASK2): remove the default texts - we do not need them any more
-      // TODO(TASK2): Add image JSX tag referring to the static image
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Image source={require('image!codepot')} style={styles.image}/>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
-  // TODO(TASK2): add style for the image and remove unused ones
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: Image.resizeMode.contain
+  }
 });
-//TODO(TASK2): Try to figure out what I have not told to do ;)
 
 AppRegistry.registerComponent('CodepotReact', () => CodepotReact);
