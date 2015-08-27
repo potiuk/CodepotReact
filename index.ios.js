12781ff08ed4663e1f45f8f378fb5876e9c80589
Jarek Potiuk <jarek@potiuk.com>
2015-08-27 11:14:33 +0200

    Resolved TODOs for TASK4

--- index.ios.js
+++ index.ios.js
@@ -14,18 +14,15 @@ var {
 } = React;
 
 var CodepotReact = React.createClass({
-  // TODO(TASK4): configure IntelliJ/WebStorm (optional but highly recommended)
-  // TODO(TASK4): add initial state to the component ( {initialState: true})
-  // TODO(TASK4): log to console when the component is about to be mounted and after it's mounted
-  // TODO(TASK4): show the state content in the console [string comprehension and object's log]
-  // TODO(TASK4): Show when rendering is done in console
-  // TODO(TASK4): set some breakpoints in those methods and observe the data
-  // TODO(TASK4): modify some data on-the fly
-  // TODO(TASK4): map workspace in Chrome debugger
-  // TODO(TASK4): enable live reload and modify some of the code in the debugger
-  // TODO(TASK4): look at react view hierarchy (Chrome react extension)
-  // TODO(TASK4): look at XCode's view hierarchy
-  // TODO(TASK4): checkout react-native and run UIExplorer
+  getInitialState() {
+    return { initialState: true}
+  },
+  componentWillMount: function() {
+    console.log(`I will mount! : ${JSON.stringify(this.state)}`)
+  },
+  componentDidMount: function() {
+    console.log("I am mounted!", this.state)
+  },
   render: function() {
     return (
       <View style={ [styles.container, styles.background] }>
